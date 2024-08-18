a3eeb22ae8764c8d0dc2cb32a739a96ab2930589
shradha78 <isrivastavashradha@gmail.com>
2024-08-18 13:08:36 +0530

    handling empty array response for EXEC after multi

--- src/main/java/RedisCommandExecutor/ExecCommand.java
+++ src/main/java/RedisCommandExecutor/ExecCommand.java
@@ -8,12 +8,19 @@ import java.util.List;
 
 import static RedisCommandExecutor.IncrCommand.sendErrorResponse;
 
-public class ExecCommand implements IRedisCommandHandler{
+public class ExecCommand implements IRedisCommandHandler {
     @Override
     public void execute(List<String> args, OutputStream outputStream) throws IOException {
-            if(!Main.queueOfCommandsForMultiAndExec.isEmpty() && !Main.queueOfCommandsForMultiAndExec.peek().getCommand().equals("MULTI")){
-                sendErrorResponse(outputStream,"EXEC without MULTI");
-            }
+        if (!Main.queueOfCommandsForMultiAndExec.isEmpty() && !Main.queueOfCommandsForMultiAndExec.peek().getCommand().equals("MULTI")) {
+            sendErrorResponse(outputStream, "EXEC without MULTI");
+        } else {
+            sendEmptyArrayResponse(outputStream,"0", "Empty array response is : ");
+        }
     }
 
+    private static void sendEmptyArrayResponse(OutputStream outputStream, String value, String debugPrintStatement) throws IOException {
+        String responseArray = "*" + value + "\r\n";
+        System.out.printf(debugPrintStatement + responseArray + "\n");
+        outputStream.write(responseArray.getBytes());
+    }
 }
