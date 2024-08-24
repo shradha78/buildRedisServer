a4ba6db6fc074c0d91d9360e00b95d021545ea81
shradha78 <isrivastavashradha@gmail.com>
2024-08-24 19:46:30 +0530

    WIP

--- src/main/java/RedisCommandExecutor/TypeCommand.java
+++ src/main/java/RedisCommandExecutor/TypeCommand.java
@@ -13,19 +13,19 @@ public class TypeCommand implements IRedisCommandHandler{
     @Override
     public void execute(List<String> args, OutputStream outputStream, ClientSession session) throws IOException {
         String key = args.get(0);
-        RedisServer.KeyValue keyValue = RedisServer.Main.storeKeyValue.get(key);
-        System.out.printf("Checking if the key is in streams : " + (Main.streams.containsKey(key)?true:false) +"\n");
-        if (!Main.streams.containsKey(key)) {
-            sendSimpleResponse(outputStream, "none");
+        if (Main.streams.containsKey(key)) {
+            sendSimpleResponse(outputStream, "stream");
+            return; // We can return early since we've found the correct type
         }
-        if(keyValue == null || keyValue.isExpired() ) {
-                RedisServer.Main.storeKeyValue.remove(key);
-                sendSimpleResponse(outputStream, "none");
-        }else {
-            if(Main.streams.containsKey(key)){
-                sendSimpleResponse(outputStream,"stream");
-                return;
-            }
+
+        // Check in storeKeyValue if it's not in streams
+        RedisServer.KeyValue keyValue = RedisServer.Main.storeKeyValue.get(key);
+        if (keyValue == null || keyValue.isExpired()) {
+            // Key doesn't exist or has expired
+            RedisServer.Main.storeKeyValue.remove(key);
+            sendSimpleResponse(outputStream, "none");
+        } else {
+            // Key exists and is not expired, assuming it's a string type
             sendSimpleResponse(outputStream, "string");
         }
     }
