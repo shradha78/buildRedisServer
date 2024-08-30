4ac38f56f11ec13d8db6ab7d40ef7096e63d66de
shradha78 <isrivastavashradha@gmail.com>
2024-08-31 01:19:56 +0530

    timeout issue wip

--- src/main/java/RedisCommandExecutor/XReadCommand.java
+++ src/main/java/RedisCommandExecutor/XReadCommand.java
@@ -12,7 +12,7 @@ import static RedisCommandExecutor.EchoCommand.sendBulkStringResponse;
 import static RedisCommandExecutor.XRangeCommand.*;
 
 public class XReadCommand implements IRedisCommandHandler{
-    private static final long POLL_INTERVAL_MS = 100;
+    private static final long POLL_INTERVAL_MS = 1;
     @Override
     public void execute(List<String> args, OutputStream outputStream, ClientSession session) throws IOException {
         int startIndex = 1;
@@ -51,7 +51,9 @@ public class XReadCommand implements IRedisCommandHandler{
             }
 
             try {
-                Thread.sleep(POLL_INTERVAL_MS); // Short delay before rechecking
+                synchronized (this) {
+                    Thread.sleep(POLL_INTERVAL_MS);
+                }// Short delay before rechecking
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt(); // Restore interrupt status
                 System.out.printf("Thread interrupted during BLOCK wait");
