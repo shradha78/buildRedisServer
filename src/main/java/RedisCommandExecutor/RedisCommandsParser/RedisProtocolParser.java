51b7d18f6d21a52f5fb56b5cff48a956bb80235a
shradha78 <isrivastavashradha@gmail.com>
2024-09-07 02:35:24 +0530

    fixing broken socket pipe error

--- src/main/java/RedisCommandExecutor/RedisCommandsParser/RedisProtocolParser.java
+++ src/main/java/RedisCommandExecutor/RedisCommandsParser/RedisProtocolParser.java
@@ -8,14 +8,19 @@ import java.util.List;
 public class RedisProtocolParser {
     public List<String> parseRESPMessage(BufferedReader br) throws IOException {
         List<String> mainMessageParts = new ArrayList<>();
-        System.out.println("IN parsing RESP Message\n");
+        System.out.println("IN parsing RESP Message");
+
         String line = br.readLine();
+
+        System.out.println("******* Next Line ");
         if(line == null || !line.startsWith("*")){
             throw new IOException("Invalid RESP message");
         }
 //        System.out.println("Client's command :: " + line);
 //        int numberOfArguments = Integer.parseInt(line.substring(1,2));//for debug through local
+
         int numberOfArguments = Integer.parseInt(line.substring(1));//In array data type after * there is number of elements
+
         for(int i = 0; i < numberOfArguments;i++){
             br.readLine(); //Skipping length arguments
             String message = br.readLine();
