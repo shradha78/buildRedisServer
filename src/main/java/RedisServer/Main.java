642075c1dccb4fb245db5059c5c374bff3e6dbf2
shradha78 <isrivastavashradha@gmail.com>
2024-09-07 02:28:08 +0530

    fixing last socket error

--- src/main/java/RedisServer/Main.java
+++ src/main/java/RedisServer/Main.java
@@ -25,8 +25,6 @@ public class Main {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
 
-        ServerSocket serverSocket = null;
-
         Socket clientSocket = null;
 
         int port = 6379;
