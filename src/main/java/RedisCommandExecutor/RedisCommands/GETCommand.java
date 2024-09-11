ac510bba92c33e8a3ed1c4ed5524273a33355e56
shradha78 <isrivastavashradha@gmail.com>
2024-09-11 20:25:49 +0530

    read a string from RDB

--- src/main/java/RedisCommandExecutor/RedisCommands/GETCommand.java
+++ src/main/java/RedisCommandExecutor/RedisCommands/GETCommand.java
@@ -2,9 +2,15 @@ package RedisCommandExecutor.RedisCommands;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
+import DataUtils.ConfigurationData;
 import DataUtils.KeyValuePairData;
+import RedisCommandExecutor.RedisParser.RdbParser;
 import RedisServer.ClientSession;
 import DataUtils.KeyValue;
 
@@ -24,6 +30,23 @@ public class GETCommand implements IRedisCommandHandler{
         String key = args.get(0);
 
         System.out.println("Key here is : " + key);
+
+        String dir = ConfigurationData.getConfigDetails("dir");
+        String file = ConfigurationData.getConfigDetails("dbfilename");
+        Path filePath = null;
+        if(dir != null && file != null) {
+            filePath = Paths.get(dir, file);
+            if (Files.exists(filePath)) {
+                try {
+                    RdbParser.load(filePath);
+                    System.out.println("File loaded");
+                } catch (Exception e) {
+                    System.out.println("Exception loading rdb file " + e);
+                    System.out.println("RDB file not found. Treating database as empty.");
+                    RedisResponses.LongParsedResponses.sendArrayRESPresponseForStrings(outputStream, new ArrayList<>());
+                }
+            }
+        }
 
         KeyValue keyValue = DataUtils.KeyValuePairData.getSpecificKeyDetails(key);
 
