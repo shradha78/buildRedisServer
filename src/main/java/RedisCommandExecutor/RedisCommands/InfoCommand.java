746417f367798df4d2d70ffe79cf44077f205d2c
shradha78 <isrivastavashradha@gmail.com>
2024-09-15 20:25:58 +0530

    adding master replid and offset keys to info of replication

--- src/main/java/RedisCommandExecutor/RedisCommands/InfoCommand.java
+++ src/main/java/RedisCommandExecutor/RedisCommands/InfoCommand.java
@@ -12,14 +12,20 @@ public class InfoCommand implements IRedisCommandHandler{
         System.out.println("INFO Command");
         String infoArgument = args.get(0);
         String role = "";
+        String master_replid = "";
+        String master_repl_offset = "";
 
         if(infoArgument.equals("replication")){
             if(DataUtils.ReplicationDataHandler.isIsReplica()){
                 role = "slave";
             }else{
                 role = "master";
+                master_replid = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+                master_repl_offset = "0";
             }
             RedisResponses.ShortParsedResponses.sendBulkStringResponse(outputStream,"role:"+role,"Info command output is : ");
+            RedisResponses.ShortParsedResponses.sendBulkStringResponse(outputStream,"master_replid:"+master_replid,"Info command output is : ");
+            RedisResponses.ShortParsedResponses.sendBulkStringResponse(outputStream,"master_repl_offset:"+master_repl_offset,"Info command output is : ");
         }
     }
 }
