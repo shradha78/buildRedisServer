8e765667c4602aef84e1d1fee179476a50244f6f
shradha78 <isrivastavashradha@gmail.com>
2024-09-17 00:00:58 +0530

    fixing error for info command

--- src/main/java/RedisCommandExecutor/RedisCommands/InfoCommand.java
+++ src/main/java/RedisCommandExecutor/RedisCommands/InfoCommand.java
@@ -1,5 +1,6 @@
 package RedisCommandExecutor.RedisCommands;
 
+import DataUtils.ReplicationDataHandler;
 import RedisReplication.RedisInstance;
 import RedisServer.ClientSession;
 
@@ -17,6 +18,7 @@ public class InfoCommand implements IRedisCommandHandler{
         if(infoArgument.equals("replication")) {
             if (DataUtils.ReplicationDataHandler.isIsReplica()) {
               redisInstance.setRole("slave");
+                ReplicationDataHandler.setIsReplica(false);
             } else {
                 redisInstance.setRole("master");
                 redisInstance.setReplicationId("8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb");
