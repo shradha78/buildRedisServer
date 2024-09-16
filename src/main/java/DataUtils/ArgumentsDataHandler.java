038ee05885cd76ab22b3f3f7ce93a4adebe26df0
shradha78 <isrivastavashradha@gmail.com>
2024-09-16 23:57:42 +0530

    replica sends ping to the master

--- src/main/java/DataUtils/ArgumentsDataHandler.java
+++ src/main/java/DataUtils/ArgumentsDataHandler.java
@@ -2,23 +2,31 @@ package DataUtils;
 
 import RedisCommandExecutor.RedisCommands.PingCommand;
 import RedisCommandExecutor.RedisParser.RedisCommand;
+import RedisReplication.RedisInstance;
 import RedisServer.Main;
 
 public class ArgumentsDataHandler {
 
     public static void handleTestArgumentsForConfigurations(String[] args) {
-        for(int i = 0 ; i < args.length ; i++) {
-            if(args[i].equals("--dir")) {
-                ConfigurationData.addConfigDetails("dir" ,args[++i]);
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("--dir")) {
+                ConfigurationData.addConfigDetails("dir", args[++i]);
             }
-            if(args[i].equals("--dbfilename")) {
-                ConfigurationData.addConfigDetails("dbfilename" ,args[++i]);
+            if (args[i].equals("--dbfilename")) {
+                ConfigurationData.addConfigDetails("dbfilename", args[++i]);
             }
-            if(args[i].equals("--port")) {
+            if (args[i].equals("--port")) {
                 ReplicationDataHandler.setPortToConnect(Integer.parseInt(args[++i]));
             }
-            if(args[i].equals("--replicaof")) {
+            if (args[i].equals("--replicaof")) {
                 ReplicationDataHandler.setIsReplica(true);
+                String replicaInfo = args[++i];
+                String[] parts = replicaInfo.split(" ");
+                if (parts.length == 2) {
+                    ReplicationDataHandler.setMaster_host(parts[0]);
+                    ReplicationDataHandler.setMaster_port(Integer.parseInt(parts[1]));
+                    ReplicationDataHandler.sendPingToMaster(ReplicationDataHandler.getMaster_host(), ReplicationDataHandler.getMaster_port());
+                }
             }
         }
     }
--- src/main/java/DataUtils/ReplicationDataHandler.java
+++ src/main/java/DataUtils/ReplicationDataHandler.java
@@ -1,8 +1,34 @@
 package DataUtils;
 
+import RedisReplication.RedisInstance;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class ReplicationDataHandler {
     private static int portToConnect = 0;
     private static boolean isReplica = false;
+    private static String master_host ="";
+    private static int master_port;
+
+    public static String getMaster_host() {
+        return master_host;
+    }
+
+    public static void setMaster_host(String master_host) {
+        ReplicationDataHandler.master_host = master_host;
+    }
+
+    public static int getMaster_port() {
+        return master_port;
+    }
+
+    public static void setMaster_port(int master_port) {
+        ReplicationDataHandler.master_port = master_port;
+    }
 
     public static void setPortToConnect(int portToConnect) {
         ReplicationDataHandler.portToConnect = portToConnect;
@@ -18,5 +44,23 @@ public class ReplicationDataHandler {
 
     public static boolean isIsReplica() {
         return isReplica;
+    }
+
+    static void sendPingToMaster(String masterHost, int masterPort) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(masterHost, masterPort), 5000); // 5 second timeout
+            OutputStream out = socket.getOutputStream();
+            try {
+                out.write("*1\r\n$4\r\nping\r\n".getBytes(StandardCharsets.UTF_8));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            out.flush();
+            System.out.println("PING sent to " + masterHost + ":" + masterPort);
+        } catch (IOException e) {
+            System.err.println("Failed to send PING to " + masterHost + ":" +
+                    masterPort);
+            e.printStackTrace();
+        }
     }
 }
