a3d330ad873452a9c9c1b83b2a046f07ea795ac2
shradha78 <isrivastavashradha@gmail.com>
2024-09-17 11:48:59 +0530

    The master receives PSYNC from the replica

--- src/main/java/RedisCommandExecutor/RedisCommands/PSYNCCommand.java
+++ src/main/java/RedisCommandExecutor/RedisCommands/PSYNCCommand.java
@@ -11,11 +11,9 @@ public class PSYNCCommand implements IRedisCommandHandler{
     @Override
     public void execute(List<String> args, OutputStream outputStream, ClientSession session) throws IOException {
         StringBuilder response = new StringBuilder();
-        response.append("$9\r\n").append("+FullSync\r\n")
-                .append("$").append(RedisServerConfig.getReplicationId().length())
-                .append("\r\n").append(RedisServerConfig.getReplicationId()).append("\r\n")
-                .append("$").append(RedisServerConfig.getReplicationOffset().length())
-                .append("\r\n").append(RedisServerConfig.getReplicationOffset()).append("\r\n");
+        response.append("+FULLRESYNC ")
+                .append(RedisServerConfig.getReplicationId()).append(" ")
+                .append(RedisServerConfig.getReplicationOffset()).append("\r\n");
         outputStream.write(response.toString().getBytes());
         outputStream.flush();
     }
