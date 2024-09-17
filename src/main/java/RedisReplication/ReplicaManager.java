5aee035b10641d126e7b2935dc5a4434f31fecfd
shradha78 <isrivastavashradha@gmail.com>
2024-09-17 13:33:38 +0530

    support propagating commands to multiple replicas.

--- src/main/java/RedisReplication/ReplicaManager.java
+++ src/main/java/RedisReplication/ReplicaManager.java
@@ -14,17 +14,17 @@ public class ReplicaManager {
     }
 
     // Remove replica if it disconnects
-    public static synchronized void removeReplica(ClientSession session) {
+    public static void removeReplica(ClientSession session) {
         replicas.remove(session);
     }
 
     // Get all connected replicas
-    public static synchronized Set<ClientSession> getConnectedReplicas() {
+    public static  Set<ClientSession> getConnectedReplicas() {
         return new HashSet<>(replicas); // return a copy to avoid concurrent modifications
     }
 
     // Send a command to all replicas
-    public static synchronized void propagateToAllReplicas(String command) {
+    public static void propagateToAllReplicas(String command) {
         for (ClientSession replica : replicas) {
             try {
                 replica.sendCommand(command);
